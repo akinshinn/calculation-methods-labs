57070291dcb15fcb3ecfad91ef2f316f46730435
Artem Dzagaryan <artem.dzagaryan@gmail.com>
2024-11-10 13:26:46 +0300

    CubicInterp

---
+++ lab4/lab4.cpp
@@ -0,0 +1,150 @@
+﻿
+#pragma once
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <math.h>
+# define pi 3.14159265358979323846
+using namespace std;
+void DisplayMatrix(vector<vector<double>> Matrix) {
+    for (auto& row : Matrix) {
+        for (auto el : row) {
+            cout << el << " ";
+        }
+        cout << endl;
+    }
+}
+
+void DisplayVector(vector<double> vec) {
+    for (auto el : vec) {
+        cout << el << " ";
+    }
+    cout << endl;
+}
+
+
+vector<double> Progonka(vector<vector<double>> matrix, vector<double> right) {
+    // matrix[i] - вектор содержащий 3 элта a_i b_i, c_i лишние нули не содержатся
+    int n = matrix.size();
+    vector<double> alpha{}, beta{}, res(n,0);
+
+    alpha.push_back(0);
+    beta.push_back(0);
+    alpha.push_back(-matrix[0][2] / matrix[0][1]);
+    beta.push_back(right[0] / matrix[0][1]);
+    for (int i = 2; i < n ; i++) {
+        double znam = matrix[i - 1][1] + matrix[i - 1][0] * alpha[i - 1];
+        alpha.push_back(-matrix[i - 1][2] / znam);
+        beta.push_back((right[i - 1] - matrix[i - 1][0] * beta[i - 1]) / znam);
+    }
+    
+    res[n-1] = (right[n - 1] - matrix[n - 1][0] * beta[n - 1]) / (matrix[n - 1][1] + matrix[n - 1][0] * alpha[n - 1]);
+    for (int k = n - 2; k >=0; k--) {
+        res[k] = res[k + 1] * alpha[k + 1] + beta[k + 1];
+    }
+    return res;
+}
+
+
+double func1(double x) {
+    return x * x;
+}
+
+double func2(double x) {
+    return 1 / (1 + x * x);
+}
+
+double func3(double x) {
+    return 1 / atan(1 + 10 * x * x);
+}
+
+double func4(double x) {
+    return pow((4 * x * x * x + 2 * x * x - 4 * x + 2), sqrt(2)) + asin(1 / (5 + x - x * x)) - 5;
+}
+
+
+pair<vector<double>, vector<double>> GenerateUniformGrid(double a, double b, int n, double(& func)(double x)) {
+    // n - количество узлов, [a, b] - отрезок интерполирования
+    vector<double> xValues{}, yValues{};
+    double h = (b - a) / n;
+    for (int i = 0; i < n + 1; i++) {
+        xValues.push_back(a + i * h);
+        yValues.push_back(func(a + i * h));
+    }
+    return { xValues, yValues };
+}
+
+
+
+pair<vector<double>, vector<double>> GenerateChebyshevGrid(double a, double b, int n, double(&func)(double x)) {
+    vector<double> xValues{}, yValues{};
+    double h = (b - a) / n;
+    for (int i = 0; i < n + 1; i++) {
+        double xcurr = (a + b) / 2 + (b - a) / 2 * cos(((2 * i + 1) * pi) / (2 * (n + 1)));
+        xValues.push_back(xcurr);
+        yValues.push_back(func(xcurr));
+    }
+    return { xValues, yValues };
+}
+
+void PrintGrid(vector<double>xValues, vector<double> yValues) {
+    for (int i = 0; i < xValues.size(); i++) {
+        cout << xValues[i] << " " << yValues[i] << endl;
+    }
+}
+
+
+vector<vector<double>> SplainInterpolationUniformGrid(double a, double b, int n, double(&func)(double x)) {
+    pair<vector<double>, vector<double>> grid = GenerateUniformGrid(a, b, n, func);
+    vector<double> xValues = grid.first, yValues = grid.second;
+    cout << "Uniform grid" << endl;
+    PrintGrid(grid.first, grid.second);
+    vector<double> A(n+1, 0), B(n+1, 0), C(n+1, 0), D(n+1, 0), H(n+1,0), G(n+1,0);
+    
+    for (int i = 1; i < n+1; i++) {
+        A[i] = yValues[i - 1];
+        H[i] = xValues[i] - xValues[i - 1];
+        G[i] = (yValues[i] - yValues[i - 1]) / H[i];
+    }
+    vector<vector<double>> SystemMatrix{};
+    vector <double> right{}, sol{};
+    SystemMatrix.push_back({ 0,2 * (H[1] + H[2]), H[2] });
+    right.push_back(3 * (G[2] - G[1]));
+    for (int i = 3; i < n; i++) {
+        SystemMatrix.push_back({ H[i - 1],2 * (H[i - 1] + H[i]),H[i] });
+        right.push_back(3*(G[i]-G[i-1]));
+    }
+    SystemMatrix.push_back({ H[n - 1],2 * (H[n - 1] + H[n]),H[n] });
+    right.push_back(3 * (G[n] - G[n - 1]));
+    sol = Progonka(SystemMatrix, right);
+
+    for (int i = 2; i < n+1; i++) {
+        C[i] = sol[i - 2];
+    }
+
+    for (int i = 1; i < n; i++) {
+        B[i] = G[i] - (C[i + 1] + 2 * C[i]) * H[i] / 3;
+        D[i] = (C[i + 1] - C[i]) / (3 * H[i]);
+    }
+    B[n] = G[n] - 2 * C[n] * H[n] / 3;
+    D[n] = -C[n] / (3 * H[n]);
+    return { A,B,C,D };
+}
+
+
+int main()
+{
+    double a1=-1, a2=-1, a3=-3, a4=-1, b1=1, b2=1, b3=3, b4=1;
+    int n1=5, n2=5, n3=5, n4=5;
+
+    vector<vector<double>> ex1 = SplainInterpolationUniformGrid(a1, b1, n1, func2);
+    cout << "Splaine coefs with Uniform Grid" << endl;
+    for (int i = 1; i <= n1; i++) {
+        cout << ex1[0][i] << " " << ex1[1][i] << " " << ex1[2][i] << " " << ex1[3][i] << endl;
+    }
+}
